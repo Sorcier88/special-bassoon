2cfd908375f1e0fe07933edbd3e4ba81bef1a0da
Sorcier88 <61395136+Sorcier88@users.noreply.github.com>
2026-01-17 13:04:26 +0100

    Update podcast_gen.py

--- podcast_gen.py
+++ podcast_gen.py
@@ -14,8 +14,8 @@ CONFIG_FILE = "playlists.json"
 COOKIE_FILE = "cookies.txt"
 
 # OBJECTIF : Nombre de SUCCÈS voulus par FICHIER RSS
-TARGET_SUCCESS_PER_FEED = 2
-SEARCH_WINDOW_SIZE = 15
+TARGET_SUCCESS_PER_FEED = 3
+SEARCH_WINDOW_SIZE = 20
 
 def get_or_create_release(repo):
     try:
